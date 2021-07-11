9ce2b0185112805aa55c6f0b0c40c393896429bc
Lionel Pineda <85717297+Snackraptor@users.noreply.github.com>
2021-07-10 22:29:21 -0400

    Delete db_cred.php

--- db_cred.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-//local constants
-define("DB_SERVER", "172.31.22.43");
-define("DB_USER", "Lionel200390096");
-define("DB_NAME", "Lionel200390096");
-define("DB_PASS", "LxQL_JRmtU");
-
-?>
